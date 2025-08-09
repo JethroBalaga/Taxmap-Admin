a54c2169c4e0abc4d0d8c14b4dbc53085cad1364
Jethro <balagajet@gmail.com>
2025-08-09 16:03:54 +0800

    Added Kind.tsx and added it to home route

---
+++ src/pages/Home.tabs/Kind.tsx
@@ -0,0 +1,23 @@
+import { 
+      IonContent, 
+      IonHeader, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Kind: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Kind Setup</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Kind;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,52 @@
-import { 
-      IonIcon, 
-      IonLabel, 
-      IonRouterOutlet, 
-      IonTabBar, 
-      IonTabButton, 
-      IonTabs, 
-  } from '@ionic/react';
+import {
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { albumsOutline, bookOutline,trailSignOutline} from 'ionicons/icons';
+import { albumsOutline, bookOutline, trailSignOutline } from 'ionicons/icons';
 import { Route, Redirect } from 'react-router';
 import Classification from './Home.tabs/Classification';
 import District from './Home.tabs/District';
-  
-  const Home: React.FC = () => {
+import Kind from './Home.tabs/Kind';
 
-    const tabs = [
-      {name:'Classification', tab:'classification',url: '/menu/home/classification', icon: bookOutline},
-      {name:'District', tab:'district', url: '/menu/home/district', icon: trailSignOutline},
-      {name:'Kind',tab:'kind', url: '/menu/home/kind', icon: albumsOutline},
-    ]
-    
-    return (
-      <IonReactRouter>
-        <IonTabs>
-          <IonTabBar slot="bottom">
+const Home: React.FC = () => {
 
-            {tabs.map((item, index) => (
-              <IonTabButton key={index} tab={item.tab} href={item.url}>
-                <IonIcon icon={item.icon} />
-                <IonLabel>{item.name}</IonLabel>
-              </IonTabButton>
-            ))}
-            
-          </IonTabBar>
+  const tabs = [
+    { name: 'Classification', tab: 'classification', url: '/menu/home/classification', icon: bookOutline },
+    { name: 'District', tab: 'district', url: '/menu/home/district', icon: trailSignOutline },
+    { name: 'Kind', tab: 'kind', url: '/menu/home/kind', icon: albumsOutline },
+  ]
+
+  return (
+    <IonReactRouter>
+      <IonTabs>
+        <IonTabBar slot="bottom">
+
+          {tabs.map((item, index) => (
+            <IonTabButton key={index} tab={item.tab} href={item.url}>
+              <IonIcon icon={item.icon} />
+              <IonLabel>{item.name}</IonLabel>
+            </IonTabButton>
+          ))}
+
+        </IonTabBar>
         <IonRouterOutlet>
-           <Route exact path="/menu/home/classification" component={Classification} />
-            <Route exact path="/menu/home/district" component={District} />
+          <Route exact path="/menu/home/classification" component={Classification} />
+          <Route exact path="/menu/home/district" component={District} />
+          <Route exact path="/menu/home/kind" component={Kind} />
 
           <Route exact path="/menu/home">
-           <Redirect to="/menu/home/classification" />
+            <Redirect to="/menu/home/classification" />
           </Route>
 
         </IonRouterOutlet>
-        </IonTabs>
-      </IonReactRouter>
-    );
-  };
-  
-  export default Home;
+      </IonTabs>
+    </IonReactRouter>
+  );
+};
+
+export default Home;
