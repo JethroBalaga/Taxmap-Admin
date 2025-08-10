ad9a74aafe52474d6e68c73c720bb6337d8d7f15
Jethro <balagajet@gmail.com>
2025-08-10 13:52:42 +0800

    Updated Classification.tsx to Dynamically render 2 global input component and 1 button component

--- src/pages/Home.tabs/Classification.tsx
+++ src/pages/Home.tabs/Classification.tsx
@@ -1,23 +1,61 @@
 import { 
-      IonContent, 
-      IonHeader, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Classification: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Classification Setup</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Classification;
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol
+} from '@ionic/react';
+import { useState } from 'react';
+import Input from '../../components/Globalcomponents/Input';
+import Button from '../../components/Globalcomponents/Button';
+
+const Classification: React.FC = () => {
+  const [code, setCode] = useState('');
+  const [classification, setClassification] = useState('');
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Classification Setup</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonGrid className="ion-padding">
+          <IonRow className="ion-justify-content-center">
+            <IonCol size="12" sizeMd="8" sizeLg="6">
+              <Input
+                label="Code"
+                value={code}
+                onChange={setCode}
+                placeholder="Enter code"
+              />
+              
+              <Input
+                label="Classification"
+                value={classification}
+                onChange={setClassification}
+                placeholder="Enter classification"
+              />
+              
+              <div className="ion-margin-top">
+                <Button
+                  variant="primary" 
+                  fullWidth
+                  onClick={() => console.log('Create clicked')}
+                >
+                  Create
+                </Button>
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Classification;
