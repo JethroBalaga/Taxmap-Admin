5d21748ba0c8b881f798c307ad705bb7e4a21700
Jethro <balagajet@gmail.com>
2025-08-13 18:33:22 +0800

    created Barangay.tsx tab

---
+++ src/pages/Home.tabs/Barangay.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+
+const Barangay: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Barangay</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        {/* Add content here later */}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Barangay;
