c3da8c7dfdc0a5d86d7ec4bb9803006dc2c7aeb1
Jethro <balagajet@gmail.com>
2025-08-14 16:44:45 +0800

    Added search and icons on barangay.tsx

--- src/pages/Home.tabs/Barangay.tsx
+++ src/pages/Home.tabs/Barangay.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+import { 
+  IonPage, 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonIcon,
+  IonSearchbar
+} from '@ionic/react';
+import { add, arrowUpCircle, trash } from 'ionicons/icons';
+import './../../CSS/Setup.css'; // Make sure this path is correct
 
 const Barangay: React.FC = () => {
   return (
@@ -10,7 +23,34 @@ const Barangay: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        {/* Add content here later */}
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12" className="search-container">
+              <IonSearchbar
+                placeholder="Search barangays..."
+                debounce={0}
+              />
+
+              <div className="icon-group">
+                <IonIcon
+                  icon={add}
+                  className="icon-yellow"
+                  title="Add Barangay"
+                />
+                <IonIcon
+                  icon={arrowUpCircle}
+                  className="icon-yellow icon-disabled"
+                  title="Edit Barangay"
+                />
+                <IonIcon
+                  icon={trash}
+                  className="icon-yellow icon-disabled"
+                  title="Delete Barangay"
+                />
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
