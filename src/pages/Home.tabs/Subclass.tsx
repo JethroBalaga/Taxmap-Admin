100f32c37d479f15f89345dfa1c036733de29c09
Jethro <balagajet@gmail.com>
2025-08-19 09:16:04 +0800

    Updated Subclass.tsx to inherit class_id

--- src/pages/Home.tabs/Subclass.tsx
+++ src/pages/Home.tabs/Subclass.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { 
   IonContent, 
   IonHeader, 
@@ -10,33 +12,42 @@ import {
   IonIcon,
   IonSearchbar
 } from '@ionic/react';
-import React, { useState, useRef} from 'react';
 import { add, arrowUpCircle, trash } from 'ionicons/icons';
 import './../../CSS/Setup.css';
 
 const Subclass: React.FC = () => {
-  // State will be added here later
-  const searchRef = useRef<HTMLIonSearchbarElement>(null);
+  const location = useLocation();
+  const [classId, setClassId] = useState<string | null>(null);
   const [searchTerm, setSearchTerm] = useState('');
 
-  // Placeholder for icon buttons (functionality to be added later)
+  // Get class_id from URL when component mounts
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const id = queryParams.get('class_id');
+    if (id) {
+      setClassId(id);
+      console.log('Received class_id:', id); // For debugging
+    }
+  }, [location]);
+
+  // Placeholder functions for buttons
   const iconButtons = [
     { 
       icon: add, 
-      onClick: () => console.log('Add clicked'), 
-      disabled: false, 
+      onClick: () => console.log('Add subclass for class', classId), 
+      disabled: !classId, 
       title: "Add Subclass" 
     },
     { 
       icon: arrowUpCircle, 
-      onClick: () => console.log('Edit clicked'), 
-      disabled: true,  // Disabled by default until a row is selected
+      onClick: () => console.log('Edit subclass for class', classId), 
+      disabled: true, 
       title: "Edit Subclass" 
     },
     { 
       icon: trash, 
-      onClick: () => console.log('Delete clicked'), 
-      disabled: true,  // Disabled by default until a row is selected
+      onClick: () => console.log('Delete subclass for class', classId), 
+      disabled: true, 
       title: "Delete Subclass" 
     },
   ];
@@ -45,7 +56,9 @@ const Subclass: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Subclass Setup</IonTitle>
+          <IonTitle>
+            {classId ? `Subclasses (Class ID: ${classId})` : 'Subclasses'}
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
 
@@ -54,7 +67,6 @@ const Subclass: React.FC = () => {
           <IonRow>
             <IonCol size="12" className="search-container">
               <IonSearchbar
-                ref={searchRef}
                 placeholder="Search subclasses..."
                 onIonInput={(e) => setSearchTerm(e.detail.value || '')}
                 debounce={0}
@@ -76,13 +88,11 @@ const Subclass: React.FC = () => {
 
           <IonRow>
             <IonCol size="12">
-              {/* DynamicTable will be added here later */}
-              <p>Table content will go here</p>
+              {/* Table content will go here */}
+              <p>Subclasses for class ID: {classId}</p>
             </IonCol>
           </IonRow>
         </IonGrid>
-
-        {/* Modals and alerts will be added here later */}
       </IonContent>
     </IonPage>
   );
