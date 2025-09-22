f5f20362a3637041adc9aabc4edf29e4697c1ec3
Jethro <balagajet@gmail.com>
2025-09-22 12:44:44 +0800

    Added Forms.tsx

---
+++ src/pages/Forms.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useRef } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonSearchbar,
+  IonIcon
+} from '@ionic/react';
+import { add } from 'ionicons/icons';
+import '../CSS/Setup.css';
+
+const Forms: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchRef = useRef<HTMLIonSearchbarElement>(null);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Forms</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12" className="search-container">
+              <IonSearchbar
+                ref={searchRef}
+                placeholder="Search forms..."
+                onIonInput={(e) => setSearchTerm(e.detail.value || '')}
+                debounce={0}
+              />
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Forms;
