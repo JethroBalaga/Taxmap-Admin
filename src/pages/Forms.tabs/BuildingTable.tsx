8fadaa98c57cedfeb1839e020f79546c64ebf550
Jethro <balagajet@gmail.com>
2025-09-22 14:59:43 +0800

    Updated BuildingTable to inherit the passdown data

--- src/pages/Forms.tabs/BuildingTable.tsx
+++ src/pages/Forms.tabs/BuildingTable.tsx
@@ -8,10 +8,21 @@ import {
 } from '@ionic/react';
 import { informationCircleOutline } from 'ionicons/icons';
 import '../../CSS/Setup.css';
+import { useLocation } from 'react-router-dom'; // Add this import
+
+interface RouteParams {
+  formId: string;
+  classId: string;
+  area: string;
+}
 
 const BuildingTable: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const searchRef = useRef<HTMLIonSearchbarElement>(null);
+  const location = useLocation(); // Add this hook
+  
+  // Get the passed parameters
+  const { formId, classId, area } = location.state as RouteParams || {};
 
   return (
     <div>
@@ -32,6 +43,15 @@ const BuildingTable: React.FC = () => {
                 title="Building information"
               />
             </div>
+          </IonCol>
+        </IonRow>
+        
+        {/* Display the passed parameters */}
+        <IonRow>
+          <IonCol>
+            <p>Form ID: {formId || 'N/A'}</p>
+            <p>Class ID: {classId || 'N/A'}</p>
+            <p>Area: {area || 'N/A'}</p>
           </IonCol>
         </IonRow>
       </IonGrid>
