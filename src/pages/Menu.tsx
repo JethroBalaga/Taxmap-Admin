b413524ed8c089412a81ecaae44cb3d9312ab3a5
Jethro <balagajet@gmail.com>
2025-09-23 22:44:02 +0800

    Added Map route to Menu.tsx

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -13,15 +13,18 @@ import {
       IonTitle, 
       IonToolbar 
   } from '@ionic/react'
-  import {documentTextOutline, homeOutline, informationOutline, logOutOutline, peopleOutline, rocketOutline} from 'ionicons/icons';
+  import {documentTextOutline, homeOutline, informationOutline, logOutOutline, mapOutline, peopleOutline, rocketOutline} from 'ionicons/icons';
 import { Redirect, Route } from 'react-router';
 import Home from './Home';
 import People from './People';
 import Forms from './Forms';
 import BuildingTable from './Forms.tabs/BuildingTable';
+import { map } from 'leaflet';
+import Map from './Map';
   const Menu: React.FC = () => {
     const path = [
         {name:'Home', url: '/menu/home', icon: homeOutline},
+        {name:'Map', url: '/menu/map', icon: mapOutline},
         {name:'People', url: '/menu/people', icon: peopleOutline},
         {name:'Forms', url: '/menu/forms', icon: documentTextOutline},
     ]
@@ -61,6 +64,7 @@ import BuildingTable from './Forms.tabs/BuildingTable';
         <IonContent className="ion-padding">
         <IonRouterOutlet id="main">
         <Route exact path="/menu/home" component={Home} />
+        <Route exact path="/menu/map" component={Map} />
         <Route exact path="/menu/people" component={People} />
         <Route exact path="/menu/forms" component={Forms} />
         <Route exact path="/menu/buildingtable" component={BuildingTable} />
