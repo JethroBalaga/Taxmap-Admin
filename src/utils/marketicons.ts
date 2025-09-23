4705615683bc3005d53c3f06a41f0dfcda5db58d
Jethro <balagajet@gmail.com>
2025-09-23 23:12:17 +0800

    Added Markericons utility

---
+++ src/utils/marketicons.ts
@@ -0,0 +1,15 @@
+// src/utils/markerIcons.ts
+import L from 'leaflet';
+import blueMarkerIconUrl from '../assets/Blue.png';
+
+export const createBlueMarkerIcon = () => {
+  return L.icon({
+    iconUrl: blueMarkerIconUrl,
+    iconSize: [45, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    shadowSize: [41, 41],
+    shadowAnchor: [12, 41]
+  });
+};
