22ae16baccdded7819789026a806ecf770d7eb21
Jethro <balagajet@gmail.com>
2025-09-28 22:50:35 +0800

    updated Machine.tsx with back button and equipment data display and receive equipment id

--- src/pages/Home.tabs/Machine.tsx
+++ src/pages/Home.tabs/Machine.tsx
@@ -10,13 +10,39 @@ import {
   IonCol,
   IonIcon,
   IonSearchbar,
+  IonButton,
+  IonButtons,
+  IonLabel,
 } from '@ionic/react';
-import { add, arrowUpCircle, trash } from 'ionicons/icons';
+import { add, arrowUpCircle, trash, arrowBack } from 'ionicons/icons';
+import { useHistory, useLocation } from 'react-router-dom';
 import './../../CSS/Setup.css';
+
+// Define the type for the location state
+interface LocationState {
+  equipmentData?: {
+    equipment_id: string;
+    machine_type: string;
+  };
+}
 
 const Machine: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const searchRef = useRef<HTMLIonSearchbarElement>(null);
+  const history = useHistory();
+  const location = useLocation();
+
+  // Get equipment_id from URL parameters
+  const queryParams = new URLSearchParams(location.search);
+  const equipmentId = queryParams.get('equipment_id');
+
+  // Get equipment data from navigation state
+  const locationState = location.state as LocationState;
+  const equipmentData = locationState?.equipmentData;
+
+  const handleBackClick = () => {
+    history.push('/menu/home/equipment');
+  };
 
   const iconButtons = [
     { icon: add, title: "Add Machine" },
@@ -28,8 +54,22 @@ const Machine: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Machine Setup</IonTitle>
+          <IonButtons slot="start">
+            <IonButton onClick={handleBackClick}>
+              <IonIcon icon={arrowBack} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle>
+            Machine Setup
+          </IonTitle>
         </IonToolbar>
+        {equipmentData && (
+          <IonToolbar>
+            <IonLabel className="structure-label">
+              Equipment: {equipmentData.equipment_id} - {equipmentData.machine_type}
+            </IonLabel>
+          </IonToolbar>
+        )}
       </IonHeader>
 
       <IonContent fullscreen>
