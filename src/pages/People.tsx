221ac4fcdd28e9e6bbbc80591dc95e41aacb0b2e
Jethro <balagajet@gmail.com>
2025-10-23 14:52:45 +0800

    removed exact path

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -7,34 +7,37 @@ import {
     IonTabButton,
     IonTabs,
 } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router'; // Add this import
 import { Route, Redirect } from 'react-router';
 import { documentOutline } from 'ionicons/icons';
 import Declarant from './People.tabs/Declarant';
 
 const People: React.FC = () => {
-
     const tabs = [
         { name: 'Declarant', tab: 'declarant', url: '/menu/people/declarant', icon: documentOutline },
     ]
     
     return (
-        <IonTabs>
-            <IonRouterOutlet>
-                <Route exact path="/menu/people/declarant" component={Declarant} />
-                <Route exact path="/menu/people">
-                    <Redirect to="/menu/people/declarant" />
-                </Route>
-            </IonRouterOutlet>
+        <IonReactRouter> {/* Add this wrapper to match Home.tsx */}
+            <IonTabs>
+                <IonRouterOutlet>
+                    <Route exact path="/menu/people/declarant" component={Declarant} />
+                    
+                    <Route exact path="/menu/people">
+                        <Redirect to="/menu/people/declarant" />
+                    </Route>
+                </IonRouterOutlet>
 
-            <IonTabBar slot="bottom">
-                {tabs.map((item, index) => (
-                    <IonTabButton key={index} tab={item.tab} href={item.url}>
-                        <IonIcon icon={item.icon} />
-                        <IonLabel>{item.name}</IonLabel>
-                    </IonTabButton>
-                ))}
-            </IonTabBar>
-        </IonTabs>
+                <IonTabBar slot="bottom">
+                    {tabs.map((item, index) => (
+                        <IonTabButton key={index} tab={item.tab} href={item.url}>
+                            <IonIcon icon={item.icon} />
+                            <IonLabel>{item.name}</IonLabel>
+                        </IonTabButton>
+                    ))}
+                </IonTabBar>
+            </IonTabs>
+        </IonReactRouter>
     );
 };
 
