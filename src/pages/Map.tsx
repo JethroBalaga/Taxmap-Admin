9b89aa692217b034ee0c280cce99628bd5fa6e06
Jethro <balagajet@gmail.com>
2025-10-26 23:57:08 +0800

    add search functionality to admin map

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,17 +1,50 @@
 import { 
   IonContent, 
+  IonHeader, 
   IonPage, 
-  IonCard
+  IonTitle, 
+  IonToolbar, 
+  IonCard,
+  IonSearchbar
 } from '@ionic/react';
+import { useState } from 'react';
 import MapCon from '../components/MapCon';
 import '../CSS/Map.css';
 
 const Map: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const handleSearch = (event: CustomEvent) => {
+    setSearchQuery(event.detail.value || '');
+  };
+
+  const handleClearSearch = () => {
+    setSearchQuery('');
+  };
+
   return (
     <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Admin Map</IonTitle>
+        </IonToolbar>
+        
+        {/* Search Bar in Header - BELOW the title */}
+        <IonToolbar>
+          <IonSearchbar
+            value={searchQuery}
+            onIonInput={handleSearch}
+            onIonClear={handleClearSearch}
+            placeholder="Search properties..."
+            className="map-searchbar"
+            animated
+          />
+        </IonToolbar>
+      </IonHeader>
+      
       <IonContent fullscreen className="map-content">
         <IonCard className="map-card">
-          <MapCon/>
+          <MapCon searchQuery={searchQuery} isAdmin={true} />
         </IonCard>
       </IonContent>
     </IonPage>
